b4dc6752ca591b2f11b07186b7e1d1fcc8b0dbf6
Alex Lane <alex.lane@quadpay.com>
2022-10-19 17:03:35 -0400

    Make menu mobile friendly

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,15 +1,38 @@
 import { MenuItem } from "../";
 import "./Menu.scss";
+import { FiMenu } from "react-icons/fi";
+import { useState, useEffect } from "react";
 
 function Menu() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isSmallScreen, setIsSmallScreen] = useState(windowWidth <= 640);
+  const [showMenu, setShowMenu] = useState(!isSmallScreen);
+  const onMenuClickHandler = () => setShowMenu(!showMenu);
+  const onMenuItemsClickHandler = () => setShowMenu(!isSmallScreen);
+  const resizeHandler = () => {
+    setWindowWidth(window.innerWidth);
+    setIsSmallScreen(windowWidth <= 640);
+    setShowMenu(!isSmallScreen);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeHandler);
+
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+    };
+  }, [windowWidth]);
+
   return (
     <div className="Menu">
-      <div className="items">
+      <FiMenu className="menuIcon" onClick={ onMenuClickHandler } />
+      <div className={showMenu ? "items" : "items hidden"} onClick={ onMenuItemsClickHandler }>
         <MenuItem label="Home" href="/" isHome={true} />
         <MenuItem label="About" href="/about" />
         <MenuItem label="Portfolio" href="/portfolio" />
         <MenuItem label="Contact" href="/contact" />
       </div>
+      
     </div>
   );
 }
