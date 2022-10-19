6b86a8dd55962a6ded2813b1f0f14031170ade34
Lyndsie Lane <lyndsielane@yahoo.com>
2022-10-19 19:21:13 -0400

    home photo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,9 @@
 import "./Home.scss";
-import homePhoto from "src/pages/Home/home photo.jpg";
 
 function Home() {
   return (
     <div>
-      <img src={require(homePhoto)} alt="home photo"></img>
+      <img src="src/pages/Home/home photo.jpg" alt="home photo"></img>
     </div>
   );
 }
