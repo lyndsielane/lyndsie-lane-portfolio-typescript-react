9e8c71f48e0416ba668481473e9f652ec0808908
Lyndsie Lane <lyndsielane@yahoo.com>
2022-10-19 18:21:11 -0400

    home photo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,11 @@
 import "./Home.scss";
 
 function Home() {
-  return <div className="Home"><This is my home page></This>;
+  return (
+    <div className="Home">
+      <img src="public/home photo.jpg"></img>
+    </div>
+  );
 }
 
 export default Home;
